393bbb511e4243be662a2d74ae35ddf930396be0
Mark Njenga <marknjenga25@gmail.com>
2024-09-07 12:07:56 +0300

    "Added Anagram class with __init__ and match methods."

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = sorted(word)
+
+    def match(self, words):
+        return [word for word in words if sorted(word) == self.sorted_word and word != self.word]
